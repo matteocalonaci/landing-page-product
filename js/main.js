5700c8878ee1895cb029eef5c739e33d6953e444
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-12-03 12:51:06 +0100

    header/ section 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
-//ok index e main sono collegate
-const title = document.querySelector(".title").style.color= "red";
+const nav = ['Home', 'i punti di forza', 'Recensioni', 'Paperelle', 'FAQs', 'Newsletter'];
+
+
+// Seleziona tutti gli <ul> nel documento
+const navLists = document.querySelectorAll("ul");
+
+const navItems = nav.map(item => {
+    return `<li>${item}</li>`; 
+});
+
+// Popola ogni <ul> con gli elementi di navigazione
+navLists.forEach(ul => {
+    ul.innerHTML = navItems.join('');
+});
+
+console.log(navLists);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
